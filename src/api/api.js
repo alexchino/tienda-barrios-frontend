b8bb9ab7c7eea38d321148656a3c753003fcfc19
Alexander canles <canalesalexander187@gmail.com>
2026-05-16 09:59:12 -0600

    Fix: Eliminando localhost y poniendo URL de Render en todo el proyecto

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL ,
   timeout: 15000,
   // 💡 Quitamos el Content-Type fijo para que Axios lo maneje dinámicamente
 });
